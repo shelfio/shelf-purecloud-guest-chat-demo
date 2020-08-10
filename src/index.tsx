2fef3ac0ecded901bf6ac8b59cf07ffe4fa9cd8d
Yaroslav <chapelskyi.slavik@gmail.com>
2020-08-10 17:25:35 +0300

    docs: remove ids

--- src/index.tsx
+++ src/index.tsx
@@ -16,17 +16,17 @@ ReactDOM.render(
           chatBotCredentials: {
             accountId: 'xxx',
             shelfDomain: 'shelf.io',
-            allowedSSPLibraryIds: ['f78c9a1e-e7d7-4b55-a8de-9d4ec18daded']
+            allowedSSPLibraryIds: ['xxx']
           },
           chatCredentials: {
-            organizationId: 'bb570674-c29a-4b70-aca1-5660981f9a22',
-            deploymentId: 'be348b1a-8644-47c2-86ec-94ab69fa1a65',
+            organizationId: 'xxx',
+            deploymentId: 'xxx',
             memberInfo: {
               displayName: 'Guest'
             },
             routingTarget: {
               targetType: 'queue',
-              targetAddress: 'Web Chat Queue'
+              targetAddress: 'xxx'
             }
           }
         }}
